2e0ced7a74ab09433570fca31196db017ffed0a9
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-09-04 16:33:58 -0300

    Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,86 @@
-import { HomeContainer } from './styles';
+import { HomeContainer, MoviesContainer } from './styles';
 
 import { FullMovieCard } from '../../components/FullMovieCard';
 
+import { useState, useEffect } from 'react';
+
+import { db } from '../../services/api';
+import config from '../../config';
+
+export interface MovieProps {
+  id: number;
+  vote_average: number;
+  title: string;
+  poster_path: string;
+}
+
+// interface MoviesListsProps {
+//   MovieProps[]
+// }
+
+interface MoviesStateProps {
+  popular: MovieProps[];
+  topRated: MovieProps[];
+  now_playing: MovieProps[];
+  upcoming: MovieProps[];
+}
+
 export const Home = () => {
+  const [movies, setMovies] = useState<MoviesStateProps>({
+    popular: [],
+    topRated: [],
+    now_playing: [],
+    upcoming: [],
+  });
+
+  useEffect(() => {
+    (async () => {
+      const popular = await db.get(`/movie/popular${config.requestConfig}`);
+      const topRated = await db.get(`/movie/top_rated${config.requestConfig}`);
+      const now_playing = await db.get(
+        `/movie/now_playing${config.requestConfig}`,
+      );
+      const upcoming = await db.get(`/movie/upcoming${config.requestConfig}`);
+      console.log(upcoming.data);
+      const allMovies = {
+        popular: [...popular.data.results],
+        topRated: [...topRated.data.results],
+        now_playing: [...now_playing.data.results],
+        upcoming: [...upcoming.data.results],
+      };
+      setMovies(allMovies);
+    })();
+  }, []);
+
   return (
     <HomeContainer>
-      <FullMovieCard />
+      <h2>Mais Populares</h2>
+      <MoviesContainer>
+        {movies.popular.map((movie: MovieProps) => (
+          <FullMovieCard key={movie.id} {...movie} />
+        ))}
+      </MoviesContainer>
+      <h2>Melhores Avaliados</h2>
+      <MoviesContainer>
+        {movies.topRated.map((movie: MovieProps) => (
+          <FullMovieCard key={movie.id} {...movie} />
+        ))}
+      </MoviesContainer>
+      <h2>Reproduzindo Agora</h2>
+      <MoviesContainer>
+        {movies.now_playing.map((movie: MovieProps) => (
+          <FullMovieCard key={movie.id} {...movie} />
+        ))}
+      </MoviesContainer>
+      <h2>Em Breve</h2>
+      <MoviesContainer>
+        {movies.upcoming.map((movie: MovieProps) => {
+          if (movie.poster_path) {
+            // Há um filme retornado pela API que não possui imagem
+            return <FullMovieCard key={movie.id} {...movie} />;
+          }
+        })}
+      </MoviesContainer>
     </HomeContainer>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,10 +2,27 @@ import styled, { css } from 'styled-components';
 
 export const HomeContainer = styled.main`
   width: 100%;
-  padding: 3rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & h2 {
+    margin-top: 2rem;
+    color: ${({ theme }) => theme.colors['base-subtitle']};
+  }
+`;
+
+export const MoviesContainer = styled.section`
+  width: 100%;
+  padding: 2rem 3rem 3rem;
 
   display: flex;
   flex-flow: row wrap;
   align-items: center;
   justify-content: space-evenly;
+  gap: 4rem;
+
+  border-bottom: 2px solid ${({ theme }) => theme.colors['base-hover']};
 `;
